52da6dfa99730d5145589a3ccbcbf01c51a0eeae
GP <ganesh.prasannah@avalara.com>
2019-08-13 14:41:59 +0530

    test: Remove stuff I cannot test.

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -23,19 +23,3 @@ test('🧮 Parser — parse() should parse a HTML stream.', async t => {
     parser.parse();
 });
 
-// test('⚗️ Parser — parse() should emit an `error` event when htmlparser2 encounters an error.', async t => {
-//     t.plan(1);
-//     const input = await readFile('./fixtures/parser_1.html');
-//     const htmlStream = toReadableStream(input);
-//     const parser = new Parser(htmlStream);
-
-//     parser.on('end', () => {
-//         t.pass('`end` event should be emitted to signal parsing complete.');
-//         t.equal(htmlStream.eventNames().length, 0, '`end` event should\'ve removed all event listeners on input stream.');
-//         t.end();
-//     });
-//     parser.on('link', url => {
-//         t.equal(url, 'https://github.com/paambaati', 'should emit the only relevant hyperlink on the page.');
-//     });
-//     parser.parse();
-// });
